fe4deb28bf8605189cb79f23fb875a39b1f6c81e
Nikolas Neofytou <nikiforialicht@gmail.com>
2025-05-19 23:00:31 +0300

    Atomic sychronisation

--- simplesync.c
+++ simplesync.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * simplesync.c
  *
@@ -6,7 +5,6 @@
  *
  * Vangelis Koukis <vkoukis@cslab.ece.ntua.gr>
  * Operating Systems course, ECE, NTUA
- *
  */
 
 #include <errno.h>
@@ -18,19 +16,16 @@
 /*
  * POSIX thread functions do not return error numbers in errno,
  * but in the actual return value of the function call instead.
- * This macro helps with error reporting in this case.
  */
 #define perror_pthread(ret, msg) \
     do                           \
     {                            \
-        errno = ret;             \
+        errno = (ret);           \
         perror(msg);             \
     } while (0)
 
 #define N 10000000
 
-/* Dots indicate lines where you are free to insert code at will */
-/* ... */
 #if defined(SYNC_ATOMIC) ^ defined(SYNC_MUTEX) == 0
 #error You must #define exactly one of SYNC_ATOMIC or SYNC_MUTEX.
 #endif
@@ -40,80 +35,54 @@
 #else
 #define USE_ATOMIC_OPS 0
 #endif
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 void *increase_fn(void *arg)
 {
-    int i;
     volatile int *ip = arg;
-
     fprintf(stderr, "About to increase variable %d times\n", N);
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         if (USE_ATOMIC_OPS)
         {
-            /* ... */
-            /* You can modify the following line */
-            ++(*ip);
-            /* ... */
+            /* Atomic increment */
+            __sync_add_and_fetch(ip, 1);
         }
         else
         {
-            /* ... */
-            /* You cannot modify the following line */
-            pthread_mutex_lock(&lock);
+            /* Unprotected increment (mutex build) */
             ++(*ip);
-            pthread_mutex_unlock(&lock);
-            /* ... */
         }
     }
     fprintf(stderr, "Done increasing variable.\n");
-
     return NULL;
 }
 
 void *decrease_fn(void *arg)
 {
-    int i;
     volatile int *ip = arg;
-
     fprintf(stderr, "About to decrease variable %d times\n", N);
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         if (USE_ATOMIC_OPS)
         {
-            /* ... */
-            /* You can modify the following line */
-            --(*ip);
-            /* ... */
+            /* Atomic decrement */
+            __sync_sub_and_fetch(ip, 1);
         }
         else
         {
-            /* ... */
-            /* You cannot modify the following line */
-            pthread_mutex_lock(&lock);
+            /* Unprotected decrement (mutex build) */
             --(*ip);
-            pthread_mutex_unlock(&lock);
-            /* ... */
         }
     }
     fprintf(stderr, "Done decreasing variable.\n");
-
     return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    int val, ret, ok;
+    int val = 0, ret;
     pthread_t t1, t2;
 
-    /*
-     * Initial value
-     */
-    val = 0;
-
-    /*
-     * Create threads
-     */
     ret = pthread_create(&t1, NULL, increase_fn, &val);
     if (ret)
     {
@@ -127,9 +96,6 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    /*
-     * Wait for threads to terminate
-     */
     ret = pthread_join(t1, NULL);
     if (ret)
         perror_pthread(ret, "pthread_join");
@@ -137,156 +103,6 @@ int main(int argc, char *argv[])
     if (ret)
         perror_pthread(ret, "pthread_join");
 
-    /*
-     * Is everything OK?
-     */
-    ok = (val == 0);
-
-    printf("%sOK, val = %d.\n", ok ? "" : "NOT ", val);
-
-    return ok;
-=======
-/*
- * simplesync.c
- *
- * A simple synchronization exercise.
- *
- * Vangelis Koukis <vkoukis@cslab.ece.ntua.gr>
- * Operating Systems course, ECE, NTUA
- *
- */
-
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-
-/*
- * POSIX thread functions do not return error numbers in errno,
- * but in the actual return value of the function call instead.
- * This macro helps with error reporting in this case.
- */
-#define perror_pthread(ret, msg) \
-    do                           \
-    {                            \
-        errno = ret;             \
-        perror(msg);             \
-    } while (0)
-
-#define N 10000000
-
-/* Dots indicate lines where you are free to insert code at will */
-/* ... */
-#if defined(SYNC_ATOMIC) ^ defined(SYNC_MUTEX) == 0
-#error You must #define exactly one of SYNC_ATOMIC or SYNC_MUTEX.
-#endif
-
-#if defined(SYNC_ATOMIC)
-#define USE_ATOMIC_OPS 1
-#else
-#define USE_ATOMIC_OPS 0
-#endif
-
-void *increase_fn(void *arg)
-{
-    int i;
-    volatile int *ip = arg;
-
-    fprintf(stderr, "About to increase variable %d times\n", N);
-    for (i = 0; i < N; i++)
-    {
-        if (USE_ATOMIC_OPS)
-        {
-            /* ... */
-            /* You can modify the following line */
-            ++(*ip);
-            /* ... */
-        }
-        else
-        {
-            /* ... */
-            /* You cannot modify the following line */
-            ++(*ip);
-            /* ... */
-        }
-    }
-    fprintf(stderr, "Done increasing variable.\n");
-
-    return NULL;
-}
-
-void *decrease_fn(void *arg)
-{
-    int i;
-    volatile int *ip = arg;
-
-    fprintf(stderr, "About to decrease variable %d times\n", N);
-    for (i = 0; i < N; i++)
-    {
-        if (USE_ATOMIC_OPS)
-        {
-            /* ... */
-            /* You can modify the following line */
-            --(*ip);
-            /* ... */
-        }
-        else
-        {
-            /* ... */
-            /* You cannot modify the following line */
-            --(*ip);
-            /* ... */
-        }
-    }
-    fprintf(stderr, "Done decreasing variable.\n");
-
-    return NULL;
-}
-
-int main(int argc, char *argv[])
-{
-    int val, ret, ok;
-    pthread_t t1, t2;
-
-    /*
-     * Initial value
-     */
-    val = 0;
-
-    /*
-     * Create threads
-     */
-    ret = pthread_create(&t1, NULL, increase_fn, &val);
-    if (ret)
-    {
-        perror_pthread(ret, "pthread_create");
-        exit(1);
-    }
-    ret = pthread_create(&t2, NULL, decrease_fn, &val);
-    if (ret)
-    {
-        perror_pthread(ret, "pthread_create");
-        exit(1);
-    }
-
-    /*
-     * Wait for threads to terminate
-     */
-    ret = pthread_join(t1, NULL);
-    if (ret)
-        perror_pthread(ret, "pthread_join");
-    ret = pthread_join(t2, NULL);
-    if (ret)
-        perror_pthread(ret, "pthread_join");
-
-    /*
-     * Is everything OK?
-     */
-    ok = (val == 0);
-
-    printf("%sOK, val = %d.\n", ok ? "" : "NOT ", val);
-
-    return ok;
->>>>>>> 5fb00c0f1fdcac9b1f9248e2bd860bb49df1aef4
+    printf("%sOK, val = %d.\n", (val == 0) ? "" : "NOT ", val);
+    return (val == 0) ? 0 : 1;
 }
